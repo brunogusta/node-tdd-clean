7b8a8b23ff6fb95f7b555b2dedb2d5fffdbbdacf
Bruno Gustavo <brugucolosil95@gmail.com>
2020-02-13 16:25:47 -0300

    test: signUp should returns statusCode 400

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,16 @@
+import { SignUpController } from './signup'
+
+describe('SignUp Controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httRequest = {
+      body: {
+        email: 'any_email',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httResponse = sut.handle(httRequest)
+    expect(httResponse.statusCode).toBe(400)
+  })
+})
