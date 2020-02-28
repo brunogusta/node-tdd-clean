92aafde8ff26bc9459efbfd174672b8032af1582
Bruno Gustavo <brugucolosil95@gmail.com>
2020-02-27 23:54:16 -0300

    refactor: change this.client conditional

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -14,7 +14,6 @@ export class SignUpController implements Controller {
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      console.log(httpRequest)
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
