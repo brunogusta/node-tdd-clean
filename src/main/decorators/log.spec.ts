12913360cb0408ed05479f1289fbfd366eceef62
Bruno Gustavo <brugucolosil95@gmail.com>
2020-03-01 18:40:43 -0300

    refactor: move sut creation to a factory method

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,23 +1,42 @@
 import { Controller, HttpResponse } from '../../presentation/protocols'
 import { LogControllerDecorator } from './log'
 
-describe('LogControllerDecorator', () => {
-  test('Should call controller handle', async () => {
-    class ControllerStub implements Controller {
-      async handle (): Promise<HttpResponse> {
-        const httpResponse: HttpResponse = {
-          statusCode: 200,
-          body: {
-            name: 'Bruno'
-          }
+interface SutTypes {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+}
+
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (): Promise<HttpResponse> {
+      const httpResponse: HttpResponse = {
+        statusCode: 200,
+        body: {
+          name: 'Bruno'
         }
-        return new Promise(resolve => resolve(httpResponse))
       }
+      return new Promise(resolve => resolve(httpResponse))
     }
-    const controllerStub = new ControllerStub()
+  }
+
+  return new ControllerStub()
+}
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeController()
+  const sut = new LogControllerDecorator(controllerStub)
+  return {
+    sut,
+    controllerStub
+  }
+}
+
+describe('LogControllerDecorator', () => {
+  test('Should call controller handle and ensure that \n' +
+  'the response of controller decorated is the same', async () => {
+    const { sut, controllerStub } = makeSut()
     const handleSpy = jest.spyOn(controllerStub, 'handle')
 
-    const sut = new LogControllerDecorator(controllerStub)
     const httpRequest = {
       body: {
         email: 'any_email@email.com',
@@ -29,5 +48,24 @@ describe('LogControllerDecorator', () => {
 
     await sut.handle(httpRequest)
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+})
+
+describe('LogControllerDecorator', () => {
+  test('Should return the same response of controller decorated', async () => {
+    const { sut } = makeSut()
+
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        name: 'any_name',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({ name: 'Bruno' })
   })
 })
