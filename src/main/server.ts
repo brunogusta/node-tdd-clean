de765f622198faf5774b90d814cb75affcf44f56
Bruno Gustavo <brugucolosil95@gmail.com>
2020-03-01 22:26:04 -0300

    chore: add dotenv

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,9 +1,12 @@
 import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
-import env from './config/env'
 
-MongoHelper.connect(env.mongoUrl)
+require('dotenv').config()
+
+const { MONGO_URL, PORT } = process.env
+
+MongoHelper.connect(MONGO_URL)
   .then(async () => {
     const app = (await import('./config/app')).default
-    app.listen(env.port, () => console.log(`Sever running at http://localhost:${env.port}`))
+    app.listen(PORT, () => console.log(`Sever running at http://localhost:${PORT}`))
   })
   .catch(console.error)
