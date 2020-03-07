32c37fd1c1e658728459fa7c5d030f19b2775af3
Bruno Gustavo <brugucolosil95@gmail.com>
2020-03-07 16:19:56 -0300

    feat: add validation to missing params in LoginController

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,7 +1,23 @@
+import { MissingParamError } from '~/presentation/errors'
+import { badRequest, serverError } from '~/presentation/helpers'
+import { ok } from '~/presentation/helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '~/presentation/protocols'
 
 export class LoginController implements Controller {
   async handle (httRequest: HttpRequest): Promise<HttpResponse> {
-    return new Promise(resolve => resolve({ statusCode: 400, body: 'email not provided' }))
+    try {
+      const requiredFields = ['email', 'password']
+
+      for (const field of requiredFields) {
+        if (!httRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
+      }
+
+      return await ok('login successfull')
+    } catch (e) {
+      // console.error(e)
+      return serverError(e)
+    }
   }
 }
