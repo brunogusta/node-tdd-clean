ef6952e82671297f8691e752cdd0dd621ad6dbe5
Bruno Gustavo <brugucolosil95@gmail.com>
2020-03-07 17:04:00 -0300

    test: add EmailValidator to LoginController

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,14 +1,35 @@
 import { MissingParamError } from '~/presentation/errors'
 import { badRequest, ok } from '~/presentation/helpers'
+import { EmailValidator } from '../signup/signup-protocols'
 import { LoginController } from './login'
 
-const makeSut = (): LoginController => {
-  return new LoginController()
+interface sutTypes {
+  sut: LoginController
+  emailValidatorStub: EmailValidator
+}
+
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: String): Boolean {
+      return true
+    }
+  }
+
+  return new EmailValidatorStub()
+}
+
+const makeSut = (): sutTypes => {
+  const emailValidatorStub = makeEmailValidator()
+  const sut = new LoginController(emailValidatorStub)
+  return {
+    sut,
+    emailValidatorStub
+  }
 }
 
 describe('Login Controller', () => {
   test('Should return 400 if no email is  provided', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         password: 'any_password'
@@ -22,7 +43,7 @@ describe('Login Controller', () => {
 
 describe('Login Controller', () => {
   test('Should return 400 if no password is  provided', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email'
@@ -35,8 +56,24 @@ describe('Login Controller', () => {
 })
 
 describe('Login Controller', () => {
+  test('Should email validator with correct email', async () => {
+    const { sut, emailValidatorStub } = makeSut()
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+    const httpRequest = {
+      body: {
+        email: 'any_email',
+        password: 'any_password'
+      }
+    }
+
+    await sut.handle(httpRequest)
+    expect(isValidSpy).toHaveBeenCalledWith(httpRequest.body.email)
+  })
+})
+
+describe('Login Controller', () => {
   test('Should return 200 if all informations are passed', async () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const fakeRequest = {
       body: {
         email: 'any_email',
