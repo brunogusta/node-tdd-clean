c49c4262abf6bd1fecb9ffdb507d95208fd89cd1
Bruno Gustavo <brugucolosil95@gmail.com>
2020-03-07 16:20:22 -0300

    test: add validation to missing params in LoginController

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,3 +1,5 @@
+import { MissingParamError } from '~/presentation/errors'
+import { badRequest, ok } from '~/presentation/helpers'
 import { LoginController } from './login'
 
 const makeSut = (): LoginController => {
@@ -7,13 +9,42 @@ const makeSut = (): LoginController => {
 describe('Login Controller', () => {
   test('Should return 400 if no email is  provided', async () => {
     const sut = makeSut()
-    const fakeRequest = {
+    const httpRequest = {
       body: {
         password: 'any_password'
       }
     }
 
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
+  })
+})
+
+describe('Login Controller', () => {
+  test('Should return 400 if no password is  provided', async () => {
+    const sut = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
+  })
+})
+
+describe('Login Controller', () => {
+  test('Should return 200 if all informations are passed', async () => {
+    const sut = makeSut()
+    const fakeRequest = {
+      body: {
+        email: 'any_email',
+        password: 'any_password'
+      }
+    }
+
     const httpResponse = await sut.handle(fakeRequest)
-    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse).toEqual(ok('login successfull'))
   })
 })
