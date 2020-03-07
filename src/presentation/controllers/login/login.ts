698dd7e500ce171201936940cdb399d12147cf79
Bruno Gustavo <brugucolosil95@gmail.com>
2020-03-07 17:03:39 -0300

    feat: add EmailValidator to LoginController

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,17 +1,31 @@
-import { MissingParamError } from '~/presentation/errors'
+import { InvalidParamError, MissingParamError } from '~/presentation/errors'
 import { badRequest, serverError } from '~/presentation/helpers'
 import { ok } from '~/presentation/helpers/http-helper'
 import { Controller, HttpRequest, HttpResponse } from '~/presentation/protocols'
+import { EmailValidator } from '../signup/signup-protocols'
 
 export class LoginController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   async handle (httRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      const { email } = httRequest.body
       const requiredFields = ['email', 'password']
 
       for (const field of requiredFields) {
         if (!httRequest.body[field]) {
           return badRequest(new MissingParamError(field))
         }
+      }
+
+      const isValid = await this.emailValidator.isValid(email)
+
+      if (!isValid) {
+        return badRequest(new InvalidParamError('email'))
       }
 
       return await ok('login successfull')
