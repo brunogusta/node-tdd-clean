cfe3db30aaaa6484436d93905840f9d0bfb970a0
Bruno Gustavo <brugucolosil95@gmail.com>
2020-03-13 22:12:01 -0300

    feat: ensure DbAuthentication calls token generator with correct id

--- src/domain/user-cases/authentication/db-authentication.ts
+++ src/domain/user-cases/authentication/db-authentication.ts
@@ -1,24 +1,31 @@
 import { HashComparer } from '~/data/protocols/criptography/hash-comparer'
+import { TokenGenerator } from '~/data/protocols/criptography/token-generator'
 import { LoadAccountByEmailRepository } from '~/data/protocols/db/load-account-by-email-repository'
 import { Authentication, AuthenticationModel } from '../protocols/authentication'
 
 export class DbAuthentication implements Authentication {
   private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
-  private readonly hashComparerStub: HashComparer
+  private readonly hashComparer: HashComparer
+  private readonly tokenGenerator: TokenGenerator
 
-  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository, hashComparerStub: HashComparer) {
+  constructor (
+    loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    hashComparer: HashComparer,
+    tokenGenerator: TokenGenerator
+  ) {
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
-    this.hashComparerStub = hashComparerStub
+    this.hashComparer = hashComparer
+    this.tokenGenerator = tokenGenerator
   }
 
   async auth (authentication: AuthenticationModel): Promise<string> {
-    const user = await this.loadAccountByEmailRepository.load(authentication.email)
-    if (!user) {
-      return null
+    const account = await this.loadAccountByEmailRepository.load(authentication.email)
+
+    if (account) {
+      await this.hashComparer.compare(authentication.password, account.password)
+      await this.tokenGenerator.generate(account.id)
     }
 
-    await this.hashComparerStub.compare(authentication.password, user.password)
-
-    return new Promise(resolve => resolve('ok'))
+    return null
   }
 }
